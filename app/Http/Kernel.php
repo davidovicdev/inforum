6487d86ef3c553a60e545c8342eb8109782e2335
Matija Davidović <davidovicdev@gmail.com>
2022-05-19 06:07:18 +0200

    deleted welcome.blade.php and added isLogged and isAdmin middlewares to Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\isAdmin;
+use App\Http\Middleware\isLogged;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -63,5 +65,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        "isLogged" => isLogged::class,
+        "isAdmin" => isAdmin::class
     ];
 }
